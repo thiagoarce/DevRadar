9f3ec6f4922734ff488101ead2d073af26a5e185
thiagoarce <thiagoarce@outlook.com>
2020-01-16 12:20:08 -0400

    CSS construído

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,145 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
-//Componente: Bloco isolado de HTML, CSS, JS, o qual não interfere no restante da aplicação
-//Propriedade: Informações que um componente PAI passa para o componente FILHO
-//Estado: Informações mantidas pelo componente (imutabilidade)
-//<></> Fragment: Usa para colocar componentes juntos
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [githubUsername, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
 
-  function incrementCounter() {
-    setCounter(counter +1)
-  }
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude.toFixed(4));
+        setLongitude(longitude.toFixed(4));
+
+
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    )
+  }, []);
+
+
+  async function handleAddDev(e){
+    e.preventDefault();
+  };
 
   return (
-    <>
-      <h1>Contador: {counter}</h1>
-      <button onClick={incrementCounter}>Incrementar</button>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do GitHub</label>
+            <input
+              name="github_username"
+              id="github_username"
+              required
+              value={githubUsername}
+              onChange={e => setGithubUsername(e.target.value)}
+            />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input
+              name="techs"
+              id="techs"
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+            />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input
+                type="number"
+                name="latitude"
+                id="latitude"
+                required
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input
+                type="number"
+                name="longitude"
+                id="longitude"
+                required value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/59902609?s=460&v=4" alt="Thiago Arce" />
+              <div className="user-info">
+                <strong>Thiago Arce</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Mestre em matemática com ênfase em estatística. Atualmente estudando e criando aplicações web com uso de javascript, node e react-native.</p>
+            <a href="https://github.com/thiagoarce">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/59902609?s=460&v=4" alt="Thiago Arce" />
+              <div className="user-info">
+                <strong>Thiago Arce</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Mestre em matemática com ênfase em estatística. Atualmente estudando e criando aplicações web com uso de javascript, node e react-native.</p>
+            <a href="https://github.com/thiagoarce">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/59902609?s=460&v=4" alt="Thiago Arce" />
+              <div className="user-info">
+                <strong>Thiago Arce</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Mestre em matemática com ênfase em estatística. Atualmente estudando e criando aplicações web com uso de javascript, node e react-native.</p>
+            <a href="https://github.com/thiagoarce">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/59902609?s=460&v=4" alt="Thiago Arce" />
+              <div className="user-info">
+                <strong>Thiago Arce</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Mestre em matemática com ênfase em estatística. Atualmente estudando e criando aplicações web com uso de javascript, node e react-native.</p>
+            <a href="https://github.com/thiagoarce">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
+    </div>
+
   );
 }
 
