759df8f447a52e6647ba05b2010ad5eae3cddd3e
thiagoarce <thiagoarce@outlook.com>
2020-01-15 23:42:46 -0400

    Entendo estado

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import Header from './Header'
+import React, { useState } from 'react';
 
 //Componente: Bloco isolado de HTML, CSS, JS, o qual não interfere no restante da aplicação
-//Propriedade: 
-//Estado:
+//Propriedade: Informações que um componente PAI passa para o componente FILHO
+//Estado: Informações mantidas pelo componente (imutabilidade)
 //<></> Fragment: Usa para colocar componentes juntos
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function incrementCounter() {
+    setCounter(counter +1)
+  }
+
   return (
     <>
-    <Header title="Dashboard"/>
-    <Header title="Titulo 2"/>
-    <Header title="Título 4"/>
+      <h1>Contador: {counter}</h1>
+      <button onClick={incrementCounter}>Incrementar</button>
     </>
   );
 }
