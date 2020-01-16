7ab39fb1e2e36afc59b308e80ba3c1c476a30884
thiagoarce <thiagoarce@outlook.com>
2020-01-15 23:34:03 -0400

    Criado Header para entendimento

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Header from './Header'
 
+//Componente: Bloco isolado de HTML, CSS, JS, o qual não interfere no restante da aplicação
+//Propriedade: 
+//Estado:
+//<></> Fragment: Usa para colocar componentes juntos
 
 function App() {
   return (
-   <h1>Oie</h1>
+    <>
+    <Header title="Dashboard"/>
+    <Header title="Titulo 2"/>
+    <Header title="Título 4"/>
+    </>
   );
 }
 
